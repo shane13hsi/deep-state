469c1ca181d93159c9a8461a9631625f0449daf8
Shane Hsi <shane13hsi@gmail.com>
2015-07-15 12:03:21 +0800

    Add logic

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-export default (a, b) => a + b;
+export default {
+  setPath(obj, path, value) {
+    var leaf = this.resolvePath(obj, path);
+    leaf.obj[leaf.name] = value;
+  },
+
+  resolvePath(obj, names) {
+    if (typeof names === 'string') {
+      names = names.split('.');
+    }
+    var lastIndex = names.length - 1;
+    var current = obj;
+    for (var i = 0; i < lastIndex; i++) {
+      var name = names[i];
+      current = current[name];
+    }
+    return {
+      obj: current,
+      name: names[lastIndex]
+    };
+  }
+};
