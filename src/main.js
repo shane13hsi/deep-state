0c0b5aa2af1612c076fbdf5f729162568f3e3de7
Shane Hsi <shane13hsi@gmail.com>
2015-07-15 14:22:12 +0800

    Remove this

--- src/main.js
+++ src/main.js
@@ -1,22 +1,27 @@
+const resolvePath = (obj, names) => {
+  if (typeof names === 'string') {
+    names = names.split('.');
+  }
+  var lastIndex = names.length - 1;
+  var current = obj;
+  for (var i = 0; i < lastIndex; i++) {
+    var name = names[i];
+    current = current[name];
+  }
+  return {
+    obj: current,
+    name: names[lastIndex]
+  };
+};
+
 export default {
-  setPath(obj, path, value) {
-    var leaf = this.resolvePath(obj, path);
-    leaf.obj[leaf.name] = value;
+  getPath(obj, path) {
+    let leaf = resolvePath(obj, path);
+    return leaf.obj[leaf.name];
   },
 
-  resolvePath(obj, names) {
-    if (typeof names === 'string') {
-      names = names.split('.');
-    }
-    var lastIndex = names.length - 1;
-    var current = obj;
-    for (var i = 0; i < lastIndex; i++) {
-      var name = names[i];
-      current = current[name];
-    }
-    return {
-      obj: current,
-      name: names[lastIndex]
-    };
+  setPath(obj, path, value) {
+    let leaf = resolvePath(obj, path);
+    leaf.obj[leaf.name] = value;
   }
 };
